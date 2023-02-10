09bdde88fe64108cf5d3d108dd0653e7008acca9
Tara Maddox <TMaddox85@gmail.com>
2023-02-10 13:46:57 -0800

    adding 404 route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,4 +6,25 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.listen(process.env.PORT)
+
+const express = require('express')
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
 app.listen(3000)
+
+require('dotenv').config()
+const express = require('express')
+
+app.get('/', (req, res) => {
+    res.status(404).send('<h1> 404 page </h1>')
+})
+
+app.get('*', (req, res) => {
+    
+})
+
+app.listen(process.env.PORT)
