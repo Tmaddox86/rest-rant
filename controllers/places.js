6d094009de7d8b476273006148d8327517c90096
Tara Maddox <TMaddox85@gmail.com>
2023-02-16 17:27:36 -0800

    new places tried to add

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,5 @@
 const router = require('express').Router()
-
-router.get('/', (req, res)=> {
-    res.send('GET /places')
-})
+const places = require('../models/places.js')
 
 let places = [{
     name: 'Amy',
@@ -30,9 +27,21 @@ app.get('/', (req,res)=> {
 res.render('places/index')
         })
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places ')
-})
+    
+        if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+          req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+          req.body.state = 'USA'
+        }
+        places.push(req.body)
+        res.redirect('POST /places')
+      })
+      
   
 router.get('/new', (req, res) => {
     res.render('GET /places/new')
