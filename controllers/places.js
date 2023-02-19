5b0665346e7e45821d9849a0e748f75b7a4d616b
Tara Maddox <TMaddox85@gmail.com>
2023-02-18 18:37:53 -0800

    part 7 done

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,17 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
+const React = require('react')
+const Def = require('../default')
+
+function show () {
+  return (
+      <Def>
+        <main>
+          <h1>Show Page</h1>
+        </main>
+      </Def>
+  )
+}
 
 let places = [{
     name: 'Amy',
@@ -22,12 +34,11 @@ app.get('/', (req,res)=> {
             state: 'AZ',
             cuisines: 'Coffee, Bakery',
             pic: 'http://placekitten.com/250/250'
-          }]
-        
-res.render('places/index',{places})
+          }] 
+          res.render('places/index',{places})
         })
+
 router.post('/', (req, res) => {
-    
         if (!req.body.pic) {
           // Default image if one is not provided
           req.body.pic = 'http://placekitten.com/400/400'
@@ -41,11 +52,10 @@ router.post('/', (req, res) => {
         places.push(req.body)
         res.redirect('POST /places')
       })
-      
-  
+
 router.get('/new', (req, res) => {
-    res.render('places/new')
-})
+      res.render('places/new')
+      })
 
 router.get('/:id', (req, res) => {
   let id = Number( req.params.id)
@@ -58,24 +68,7 @@ router.get('/:id', (req, res) => {
   else {
     res.render('places/show',{ place: places[id], id})
   }
-})
-
-const React = require('react')
-const Def = require('../default')
-
-function show () {
-    return (
-        <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
-        </Def>
-    )
-}
-
-router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id')
-})
+});
 
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -105,10 +98,6 @@ router.get('/:id/edit', (req, res) => {
   }
 });
 
-router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant');
-});
-
 router.delete('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)){
@@ -123,6 +112,39 @@ router.delete('/:id/edit', (req, res) => {
     res.render('STUB DELETE /places/:id/edit')
   }
 });
+
+router.post('/:id/edit', (req, res) => {
+    res.send('GET /places/:id/edit');
+});
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+
 
 
 
